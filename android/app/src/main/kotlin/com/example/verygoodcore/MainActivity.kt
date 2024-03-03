3488ebafcd337deff14fcc9d53ad42cadcbaa493
Anthony S <aymswick@Anthonys-MacBook-Pro.local>
2024-03-02 19:21:47 -0600

    Init flutter app

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.hackspace
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:hackspace/app/app.dart';
+import 'package:hackspace/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
